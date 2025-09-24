8b6fa8854dc434bc91e63f9b941c53242061880d
MRAlirad <m.r.alirad75@gmail.com>
2025-09-24 23:52:12 +0330

    Badge Component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+
+const Badge = ({ text, color = "blue", size = "medium", className = '' }: BadgeProps) => {
+	return (
+		<span
+			className={classNames({
+				badge: true,
+				[color]: true,
+				[size]: true,
+				[className]: className,
+			})}
+		>
+			{text}
+		</span>
+	);
+};
+
+interface BadgeProps {
+	text: string;
+	color?: "green" | "blue" | "neutral" | "red" | "yellow";
+	size?: "small" | "medium" | "large";
+	className?: string;
+}
+
+export default Badge;
