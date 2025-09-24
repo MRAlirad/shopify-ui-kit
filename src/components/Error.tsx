3fcc0f6723881d17afd9aabe6554e25ecd47a9d1
MRAlirad <m.r.alirad75@gmail.com>
2025-09-25 01:20:46 +0330

    Error Message Component

---
+++ src/components/Error.tsx
@@ -0,0 +1,16 @@
+import { WarningCircleIcon } from "./icon";
+
+export const ErrorMessage = ({ error = '' }: { error: string | string[] }) => {
+	if (typeof error === "string") return <ErrorText error={error} />;
+
+	return error?.map((err, index) => <ErrorText key={index} error={err} />);
+};
+
+const ErrorText = ({ error = "" }: { error: string }) => {
+	return (
+		<div className="flex items-center gap-1.5 text-red-600 text-xs ms-2">
+			<WarningCircleIcon size={15} />
+			<span>{error}</span>
+		</div>
+	);
+};
