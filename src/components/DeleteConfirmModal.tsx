58cd83181acc8fc0b8e13e35bb8aea88be0e6a84
MRAlirad <m.r.alirad75@gmail.com>
2025-09-26 21:35:11 +0330

    Delete confirm modal

---
+++ src/components/DeleteConfirmModal.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import { useLockBodyScroll, useToggle } from "react-use";
+import Button from "./Button";
+import { CloseIcon, WarningCircleIcon } from "./icon";
+
+const DeleteModal = ({ title, onClose, onDelete, isDeleting = false }: DeleteModalProps) => {
+	const [locked, toggleLocked] = useToggle(false);
+	useLockBodyScroll(locked);
+
+	useEffect(() => {
+		toggleLocked(true);
+		return () => toggleLocked(false);
+	}, [toggleLocked]);
+
+	return createPortal(
+		<div
+			className="fixed inset-0 flex items-end md:items-center justify-center bg-black/60 z-[1500] backdrop-blur-sm"
+			onClick={(e) => {
+				if (e.target === e.currentTarget) onClose();
+			}}
+		>
+			<div className="w-full max-w-md bg-white max-h-full p-4 rounded-lg overflow-hidden shadow animate-fadeInUp">
+				<Button color="simple" size="small" icon={<CloseIcon size={20} />} onClick={onClose} className="text-neutral-500 ms-auto" />
+
+				<div className="flex flex-col items-center gap-4 p-4 text-center">
+					<WarningCircleIcon size="100" className="text-neutral-400" />
+
+					<h3 className="text-lg text-neutral-700">{title}</h3>
+					<p className="text-sm text-red-500">این کار غیر قابل بازگشت است!</p>
+
+					<div className="flex items-center justify-center gap-4">
+						<Button color="white" text="خیر، انصراف" size="medium" onClick={onClose} />
+						<Button color="red" text="بله، مطمئنم" size="medium" onClick={onDelete} loading={isDeleting} />
+					</div>
+				</div>
+			</div>
+		</div>,
+		document.body
+	);
+};
+
+interface DeleteModalProps {
+	title: string;
+	onClose: () => void;
+	onDelete: () => void;
+	isDeleting: boolean;
+}
+
+export default DeleteModal;
