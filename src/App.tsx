0974dace4f6c650410aaef970055eace28e868c8
m.reza.alirad <m.reza.alirad@grg.paarian.com>
2025-10-25 17:31:37 +0330

    user 2 table list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Page from "./components/Page";
 import UserTableList from "./UserTableList";
+// import User2TableList from "./User2TableList";
 
 function App() {
 	return (
 		<Page type="shrink">
 			<UserTableList />
+			{/* <User2TableList /> */}
 		</Page>
 	);
 }
