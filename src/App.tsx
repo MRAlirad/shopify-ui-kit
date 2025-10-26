fcad13faf1eff843b566498898423d69004074df
m.reza.alirad <m.reza.alirad@grg.paarian.com>
2025-10-26 11:15:12 +0330

    Component / Line Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import LineChart from "./components/chart/LineChart";
 import Page from "./components/Page";
-import UserTableList from "./UserTableList";
+// import UserTableList from "./UserTableList";
 // import User2TableList from "./User2TableList";
 
 function App() {
 	return (
 		<Page type="shrink">
-			<UserTableList />
-			{/* <User2TableList /> */}
+			<LineChart
+				labels={["January", "February", "March", "April", "May", "June", "July"]}
+				datasets={[
+					{ label: "My First dataset", data: [65, 100, 80, 81, 56, 55, 40] },
+					{ label: "My second dataset", data: [4, 59, 5, 4, 34, 34, 5] },
+				]}
+				options={{ title: "My Chart" }}
+			/>
 		</Page>
 	);
 }
