9643c33b5cde7770e9ff14f5b7795446c571330d
m.reza.alirad <m.reza.alirad@grg.paarian.com>
2025-11-02 15:20:31 +0330

    Bug / Table Context

--- src/components/table/services/TableContext.ts
+++ src/components/table/services/TableContext.ts
@@ -1,17 +1,22 @@
 import React from "react";
+import type { ColumnProps, ActionProps } from "./Props";
 
-interface TableContextType {
-	columns: [];
-	actions: [];
+interface TableContextType<T> {
+	columns: ColumnProps<T>[];
+	actions: ActionProps<T>[];
 	searchPanel: boolean;
 	selectable: boolean;
 	moreInfo: boolean;
-	filterType: 'text' | 'select' | 'date' | 'number';
-	type: 'local' | 'remote';
+	type: "local" | "remote";
 	allowedPageSizes: number[];
 	columnHidingEnabled: boolean;
 }
 
-const TableContext = React.createContext<TableContextType>({} as TableContextType);
+function createTableContext<T>() {
+	return React.createContext<TableContextType<T>>({} as TableContextType<T>);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const TableContext = createTableContext<any>();
 
 export default TableContext;
